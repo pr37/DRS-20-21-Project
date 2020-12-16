c037635ccf228c20e699e249d8395f34b8e986b4
dejan.subasic <dejan.subasic@gmail.com>
2020-12-16 19:24:55 +0100

    board paint event

--- Board.py
+++ Board.py
@@ -27,6 +27,19 @@ class Board(QFrame):
     def square_height(self):
         return self.contentsRect().height() / Board.HEIGHTINBLOCKS
 
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        rect = self.contentsRect()  #povrsina izmedju margina widgeta
+        boardtop = rect.bottom() - Board.HEIGHTINBLOCKS * self.square_height()
+
+        for pos in self.snake.snakePosition:  #stavi prva dva kvadrata zmije na board pocetna poz
+            self.draw_square(painter, rect.left() + pos[0] * self.square_width(),
+                             boardtop + pos[1] * self.square_height())
+
+    def draw_square(self, painter, x, y): #crta kockicu zmijice
+        color = QColor(0x302213)
+        painter.fillRect(x + 1, y + 1, self.square_width() - 2,
+                        self.square_height() - 2, color)
 
     def keyPressEvent(self, event) -> None: self.Movement.keyPressEvent(self, event)
 
