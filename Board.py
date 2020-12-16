83978586d2566e5e3de441ba529dfecaa7911a6b
dejan.subasic <dejan.subasic@gmail.com>
2020-12-16 19:24:13 +0100

    Board key press event

--- Board.py
+++ Board.py
@@ -1,6 +1,36 @@
+from PyQt5.QtCore import QBasicTimer, Qt
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QFrame
-
+from Movement import Movement
+from Snake import Snake
 
 class Board(QFrame):
-    def init(self):
-        self.snake = []
+    SPEED = 150
+    WIDTHINBLOCKS = 60
+    HEIGHTINBLOCKS = 40
+
+    def __init__(self, parent):
+        super(Board, self).__init__(parent)
+        self.timer = QBasicTimer()
+        self.snake = Snake()
+        self.board = []
+        self.direction = 1
+        self.setFocusPolicy(Qt.StrongFocus)
+        self.Movement = Movement()
+
+    def start(self):
+        self.timer.start(Board.SPEED, self) #na 150 msec radi tajmer
+
+    def square_width(self):
+        return self.contentsRect().width() / Board.WIDTHINBLOCKS
+
+    def square_height(self):
+        return self.contentsRect().height() / Board.HEIGHTINBLOCKS
+
+
+    def keyPressEvent(self, event) -> None: self.Movement.keyPressEvent(self, event)
+
+    def timerEvent(self, event):
+        if event.timerId() == self.timer.timerId():
+            self.Movement.move_snake(self)
+            self.update()
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from PyQt5 import QtWidgets
+from SnakeGame import SnakeGame
 
 if __name__ == '__main__':
     app = QApplication([])
-    widget = QtWidgets.QWidget()
-    widget.resize(300, 200)
-    widget.setWindowTitle("Snake")
-    widget.show()
+    game = SnakeGame()
     sys.exit(app.exec_())
