338e8fc2c253b0a3d3e402d234a7a0c57a9695b1
Cogi987 <urosworkonly@gmail.com>
2020-12-16 18:59:05 +0100

    QMainWindow - added image, set geometry

--- SnakeGame.py
+++ SnakeGame.py
@@ -3,4 +3,16 @@ from PyQt5.QtWidgets import QMainWindow
 
 class SnakeGame(QMainWindow):
     def init(self):
-        self.setStyleSheet("QWidget {background-image: url(grass.jpg)}")
+        super(SnakeGame, self).__init__()
+        self.sboard = Board(self)
+
+        self.setCentralWidget(self.sboard)
+        self.setWindowTitle('DRS Snake Game')
+        self.setStyleSheet("QWidget {background-image: url(background.jpg)}")
+        self.resize(600, 400)
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2)
+
+        self.sboard.start()
+        self.show()
