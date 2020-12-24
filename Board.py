48c80015c095eb00906b883d6357598aa0f094b3
dejan.subasic <dejan.subasic@gmail.com>
2020-12-24 08:46:16 +0100

    board ctor fill game with numberOfPlayers each with 3 snakes

--- Board.py
+++ Board.py
@@ -1,25 +1,44 @@
 from PyQt5.QtCore import QBasicTimer, Qt
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QFrame
+import random
+
+from Config import Config
+
 from Movement import Movement
-from Snake import Snake
+from Player import Player
 from Drawer import Drawer
+
 
 class Board(QFrame):
     SPEED = 150
     WIDTHINBLOCKS = 60
     HEIGHTINBLOCKS = 40
+    Timer = QBasicTimer()
+    Players = []
+    Movement = Movement()
+    Drawer = Drawer()
 
-    def __init__(self, parent):
+    def __init__(self, parent, numberOfPlayers):
         super(Board, self).__init__(parent)
-        self.timer = QBasicTimer()
-        self.snake = Snake()
-        self.snakePosition = self.snake.snakePosition
-        self.board = []
-        self.direction = 1
+        self.Foods = []
+        self.config = Config()
         self.setFocusPolicy(Qt.StrongFocus)
-        self.Movement = Movement()
-        self.drawer = Drawer(snakePos=self.snake.snakePosition)
+        self.numberOfPlayers = numberOfPlayers
+
+        self.Grid = self.createGrid()
+
+        for i in range(numberOfPlayers):
+            offset = i * 10
+            snake1 = [[5 + offset, 13], [5 + offset, 14], [5 + offset, 15]]
+            snake2 = [[9 + offset, 13], [9 + offset, 14], [9 + offset, 15]]
+            snake3 = [[13 + offset, 13], [13 + offset, 14], [13 + offset, 15]]
+            positions = [snake1, snake2, snake3]
+            directions = [1, 1, 1]
+            self.Players.append(Player(self, i, 3, positions, directions))
+
+        self.turnPlayer = self.Players[0]
+        self.generateStartingFood()
 
     def start(self):
         self.timer.start(Board.SPEED, self) #na 150 msec radi tajmer
