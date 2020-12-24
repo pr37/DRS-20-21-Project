7aa7e7199f7e20c9c16c76be33aceaee45920e70
Dragana Trivunovic <dragana.trivunovic@vega.local@git config --global user.email dragana.trivunovic@vega.local>
2020-12-24 08:45:59 +0100

    Class FormWidget defines side controls

--- SnakeGame.py
+++ SnakeGame.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QMainWindow, QDesktopWidget
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from Board import Board
+from Player import Player
 
 
 class SnakeGame(QMainWindow):
@@ -17,3 +19,33 @@ class SnakeGame(QMainWindow):
 
         self.sboard.start()
         self.show()
+
+
+class FormWidget(QWidget):
+    def __init__(self, parent):
+        super(FormWidget, self).__init__()
+        self.parent = parent
+        self.layout = QVBoxLayout(self)
+        self.labelPlayer = QLabel("Playing : Player " + str(parent.sboard.turnPlayer.Name))
+        self.labelPlayersNum = QLabel("Number of players: " + str(parent.sboard.numberOfPlayers))
+        self.button1 = QPushButton("End turn")
+        self.button1.setStyleSheet("QPushButton{"
+                                      "color: white; background-color: green; font:bold; border-style: outset; border-width: 2px; border-color: grey"
+                                      "}"
+                                      "QPushButton:hover{"
+                                      "background-color: #3F7FBF"
+                                      "}")
+        self.button1.resize(20, 30)
+        self.button1.move(50, 50)
+        self.button1.clicked.connect(self.endTurnClick)
+        self.layout.addWidget(self.labelPlayersNum)
+        self.layout.addWidget(self.labelPlayer)
+        self.layout.addWidget(self.button1)
+        #self.button2 = QPushButton("Button 2")
+        #self.layout.addWidget(self.button2)
+
+        self.setLayout(self.layout)
+
+    def endTurnClick(self):
+        self.parent.sboard.turnPlayer.endTurn()
+        self.labelPlayer.setText("Playing : Player " + str(self.parent.sboard.turnPlayer.Name))
