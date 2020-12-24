b686ec14ca3848d5477af6e43f01f0c40bf93ed0
Dragana Trivunovic <dragana.trivunovic@vega.local@git config --global user.email dragana.trivunovic@vega.local>
2020-12-24 08:56:20 +0100

    Added config properties

--- Config.py
+++ Config.py
@@ -1,2 +1,11 @@
-class Config():
-    MAXSnakesPerPlayer = 3
+from PyQt5.QtGui import QPainter, QColor
+
+class Config:
+    def __init__(self):
+        #super(Config, self).__init__(parent)
+        self.MAXSnakesPerPlayer = 3
+        self.snakeColor1 = QColor(0x203b27)
+        self.snakeColor2 = QColor(0x2e2abd)
+        self.snakeColor3 = QColor(0x2bb5b5)
+        self.snakeColor4 = QColor(0xe83023)
+        self.startingFoodCount = 5
