5b78f312d88643bd52504e90d885c6123c592fa2
Dragana Trivunovic <dragana.trivunovic@vega.local@git config --global user.email dragana.trivunovic@vega.local>
2021-01-05 16:55:34 +0100

    started on client initial structure

---
+++ client.py
@@ -0,0 +1,90 @@
+# Client connects to server socket. It runs the main app and gets its data from it, then
+# sends it out to server so that all client instances can be updated.
+# HOW TO: Run -> Edit configuration -> Allow parallel run -> pokreni vise instanci
+
+import sys
+import socket
+import selectors
+import types
+from multiprocessing import Process
+
+from PyQt5.QtWidgets import QApplication
+from SnakeGame import SnakeGame
+
+sel = selectors.DefaultSelector()
+messages = [b"Message 1 from client.", b"Message 2 from client."] #this will be changed with data from the game
+#game data variables...
+#
+#
+
+def start_connections(host, port, num_conns):
+    server_addr = (host, port)
+    for i in range(0, num_conns):
+        connid = i + 1
+        print("starting connection", connid, "to", server_addr)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            connid=connid,
+            msg_total=sum(len(m) for m in messages),
+            recv_total=0,
+            messages=list(messages),
+            outb=b"",
+        )
+        sel.register(sock, events, data=data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            print("received", repr(recv_data), "from connection", data.connid)
+            data.recv_total += len(recv_data)
+        if not recv_data or data.recv_total == data.msg_total:
+            print("closing connection", data.connid)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print("sending", repr(data.outb), "to connection", data.connid)
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+
+# call the main app here
+
+host = "127.0.0.1"  # Standard loopback interface address (localhost)
+port = 65432  # Port to listen on (non-privileged ports are > 1023)
+num_conns = 2  # This is supposed to be equal to number of players !!!!!!!!!!!!!!!!!!!!!!
+start_connections(host, int(port), int(num_conns))
+
+def start_game():
+    app = QApplication([])
+    game = SnakeGame()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    p = Process(target=start_game)      #main game is a separate process
+    p.start()
+    p.join()
+
+try:
+    while True:
+        events = sel.select(timeout=1)
+        if events:
+            for key, mask in events:
+                service_connection(key, mask)
+        # Check for a socket being monitored to continue.
+        if not sel.get_map():
+            break
+except KeyboardInterrupt:
+    print("caught keyboard interrupt, exiting")
+finally:
+    sel.close()
